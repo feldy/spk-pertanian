4156001026927010a08a93a350601aef05e72646
Feldy Yusuf <f3ldy10rpl@gmail.com>
2014-05-19 11:22:56 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="lib/jquery/jquery-ui.css">
+        <script type="javascript" src="lib/jquery/jquery-ui.js"></script>
+    	<title>Aplikasi SPK</title>
+	</head>
+	<body>
+		haloo JQUERYee
+	</body>
+</html>
